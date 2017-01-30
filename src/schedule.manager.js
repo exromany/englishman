e810c4312a96453b391768e31e37c0dee4c0c84f
roman.uferov <roman.uferov@piano.io>
2017-01-31 00:02:17 +0400

    update schedule parser

--- src/schedule.manager.js
+++ src/schedule.manager.js
@@ -12,6 +12,7 @@ const CREDS = {
   private_key: (process.env.GOOGLE_CREDS_KEY || '').replace(/\\n/g, `\n`).replace(/^[\"\']+|[\"\']+$/g, '')
 };
 const SYNC_DELAY = parseInt(process.env.SYNC_DELAY, 10) || 60000;
+const DAY_ROWS = 9;
 
 class ScheduleManager {
   constructor() {
@@ -65,40 +66,61 @@ class ScheduleManager {
   }
 
   loadSheet(sheet) {
-    const date = chrono.parseDate(sheet.title);
-    if (!date) {
+    if (!sheet.title.endsWith('week of the year')) {
       return undefined;
     }
-    const url = sheet._links['http://schemas.google.com/visualization/2008#visualizationApi'].replace('gviz/tq?','#');
+    const url = ScheduleManager.getSheetUrl(sheet);
+    const sheetId = sheet.id;
+
+    const byDay = (num) => (cell) => cell.row >= (num - 1) * DAY_ROWS + 1 && cell.row <= num * DAY_ROWS;
+    const byCol = (col) => (cell) => cell.col === col;
+    const byRow = (row) => (cell) => cell.row % DAY_ROWS === row;
+    const byRowGE = (row) => (cell) => cell.row % DAY_ROWS >= row;
+    const byPos = (col, row) => (cell) => cell.col === col && cell.row % DAY_ROWS === row;
+    const getValue = (cell) => cell ? cell.value : null;
+
+    const readLesson = ({ url, sheetId, parseTime, trainer }) => (cells) => {
+      const time = parseTime(getValue(cells.find(byRow(2))));
+      return {
+        start: time && time.start.date() || null,
+        end: time && time.end && time.end.date() || null,
+        topic: getValue(cells.find(byRow(3))),
+        users: cells.filter(byRowGE(4)).map(cell => cell.value),
+        trainer,
+        cellCol: cells[0].col,
+        cellRow: Math.floor((cells[0].row - 1) / DAY_ROWS) * DAY_ROWS + 1,
+        sheetId,
+        url
+      };
+    };
+
+    const readDay = ({ url, sheetId }) => (cells) => {
+      const trainer = getValue(cells.find(byPos(1, 1)));
+      const date = chrono.parseDate(getValue(cells.find(byPos(2, 1))));
+      const parseTime = (text) => chrono.parse(text.replace(/\./g, ':'), date)[0];
+
+      return [1, 2, 3]
+        .map(num => cells.filter(byCol(num)))
+        .map(readLesson({ url, sheetId, parseTime, trainer }));
+    };
+
+    const readDays = ({ url, sheetId }) => (cells) => {
+      return [1, 2]
+        .map(num => cells.filter(byDay(num)))
+        .map(readDay({ url, sheetId }))
+        .reduce((list, items) => list.concat(items), []);
+    };
+
     return lift(sheet.getCells)({ 'return-empty': false })
-      .then(cells => {
-        const lessons = {};
-        cells.forEach(cell => {
-          switch (cell.row) {
-            case 1:
-              const times = chrono.parse(cell.value.replace(/\./g, ':'), date);
-              return lessons[cell.col] = {
-                start: times && times.length && times[0].start.date() || null,
-                end: times && times.length && times[0].end && times[0].end.date() || null,
-                topic: '',
-                users: [],
-                url: url,
-                sheetId: sheet.id,
-                cellCol: cell.col
-              };
-            case 2: return lessons[cell.col] && (lessons[cell.col].topic = cell.value);
-            default: return lessons[cell.col] && lessons[cell.col].users.push(cell.value);
-          }
-        });
-        return Object.keys(lessons).map(col => lessons[col]);
-      });
+      .then(readDays({ url, sheetId }));
   }
 
   loadCells(lesson) {
     return this.docInit
       .then(doc => lift(doc.getCells)(lesson.sheetId, {
         'return-empty': true,
-        'min-row': 3,
+        'min-row': lesson.cellRow + 3,
+        'max-row': lesson.cellRow + DAY_ROWS - 1,
         'min-col': lesson.cellCol,
         'max-col': lesson.cellCol
       }));
@@ -107,7 +129,9 @@ class ScheduleManager {
   addUserToLesson(name, lesson) {
     return this.loadCells(lesson)
       .then(cells => cells.find(cell => !cell.value))
-      .then(cell => lift(cell.setValue)(name));
+      .then(cell => cell && lift(cell.setValue)(name) || Promise.reject('No empty cells'))
+      .then(() => this.log('user:add', name, lesson.start))
+      .catch(err => (this.logError(err), Promise.reject(err)));
   }
 
   removeUserFromLesson(name, lesson){
@@ -115,9 +139,30 @@ class ScheduleManager {
     return this.loadCells(lesson)
       .then(cells => {
         const index = cells.findIndex(cell => cell.value === name);
-        const lastIndex = cells.findIndex(cell => !cell.value);
+        if (index < 0) {
+          return Promise.reject('Name not found');
+        }
+        const lastIndex = cells.findIndex(cell => !cell.value) + 1 || DAY_ROWS;
         return Promise.all(cells.slice(index, lastIndex).map(shiftCells));
-      });
+      })
+      .then(() => this.log('user:remove', name, lesson.start))
+      .catch(err => (this.logError(err), Promise.reject(err)));
+  }
+
+  findLessonByDate(date) {
+    const m = moment(date);
+    return this.lessons.find(lesson => m.isSame(lesson.start));
+  }
+
+  findLessonsByDateRange([date1, date2]) {
+    const m1 = moment(date1);
+    const m2 = moment(date2);
+    return this.lessons
+      .filter(lesson => m1.isSameOrBefore(lesson.start) && m2.isSameOrAfter(lesson.start));
+  }
+
+  static getSheetUrl(sheet) {
+    return sheet._links['http://schemas.google.com/visualization/2008#visualizationApi'].replace('gviz/tq?','#');
   }
 }
 
